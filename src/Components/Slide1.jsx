0644ab0cfd88a0e11f4c34008880149b7516d0a8
Iftekhar-007 <shawonsp534@gmail.com>
2025-05-21 22:52:20 +0600

    slider fully done

--- src/Components/Slide1.jsx
+++ src/Components/Slide1.jsx
@@ -12,10 +12,10 @@ const Slide1 = () => {
         <div className="hero-overlay"></div>
         <div className="hero-content text-neutral-content text-center">
           <div className="">
-            <h1 className="mb-5 text-5xl font-bold">
+            <h1 className="mb-5 text-7xl font-bold font-roboto leading-20">
               Discover New Passions Near You
             </h1>
-            <p className="mb-5">
+            <p className="mb-5 font-roboto text-base">
               From painting to photography, gardening to gaming—find local
               groups that share your <br /> interests and make new friends along
               the way
--- src/Components/Slide2.jsx
+++ src/Components/Slide2.jsx
@@ -13,10 +13,10 @@ const Slide2 = () => {
         <div className="hero-overlay"></div>
         <div className="hero-content text-neutral-content text-center">
           <div className="">
-            <h1 className="mb-5 text-5xl font-bold">
+            <h1 className="mb-5 text-7xl font-bold font-roboto leading-20">
               Join Hobby Communities <br /> That Inspire You
             </h1>
-            <p className="mb-5">
+            <p className="mb-5 font-roboto text-base">
               Connect with like-minded people in your area. Whether you're a
               beginner or <br /> an expert, there's a place for you to grow{" "}
               <br /> and thrive.
