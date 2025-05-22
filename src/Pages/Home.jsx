5dfb443c049c703f693c35ed3d32215dcbf401e9
Iftekhar-007 <shawonsp534@gmail.com>
2025-05-23 00:55:39 +0600

    okay

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Sliders from "../Components/Sliders";
 
 const Home = () => {
+  const [theme, setTheme] = useState("light");
   useEffect(() => {
     document.title = "Home | HobbyHub";
 
@@ -18,11 +19,27 @@ const Home = () => {
     setFavicon("../../public/icons8-home-100.png");
   }, []);
 
+  useEffect(() => {
+    document.title = "Home | HobbyHub";
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+  };
+
   // https://e1.pngegg.com/pngimages/630/550/png-clipart-metrostation-black-home-icon-thumbnail.png
 
   return (
-    <div>
-      <Sliders></Sliders>
+    <div className="min-h-screen">
+      <div className="flex justify-end p-4 w-[1440px] mx-auto">
+        <button className="btn btn-sm btn-outline" onClick={toggleTheme}>
+          Switch to {theme === "light" ? "Dark" : "Light"} Mode
+        </button>
+      </div>
+
+      <Sliders />
     </div>
   );
 };
